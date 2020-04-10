1dce6d4a2f57e5e1ddbd0be8ac6ad23af127060b
j721 <juliadvu@gmail.com>
2020-04-10 11:24:05 -0500

    got one of the tests working for navbar to display

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Navbar from './components/Navbar';
+import { render } from "@testing-library/react";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test ("displays navbar",()=>{
+  const {getByTestId} = render (<Navbar/>);
+  getByTestId(/nav/i)
+})
